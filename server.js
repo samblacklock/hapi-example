b10ab6ffb34a13e38a89fa232b17f694232d5c2f
samblacklock <samblacklock@me.com>
2016-06-29 21:42:34 +0100

    finished

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import Hapi from 'hapi';
 import Inert from 'inert';
 import Vision from 'vision';
-import Joi from 'joi';
-import Boom from 'boom';
 import Good from 'good';
+import HapiAuthCookie from 'hapi-auth-cookie';
 
-import uuid from 'uuid';
-import fs from 'fs';
+import Routes from './lib/routes';
+import CardStore from './lib/cardStore';
+import UserStore from './lib/userStore';
 
 const server = new Hapi.Server();
-const cards = loadCards();
+
+CardStore.initialize();
+UserStore.initialize();
 
 server.connection({ port: 3000 });
 
@@ -19,7 +21,7 @@ server.register([Inert, Vision], (err) => {
   }
 });
 
-server.register({
+/*server.register({
   register: Good,
   options: {
     opsInterval: 5000,
@@ -53,7 +55,21 @@ server.register({
       }
     ]
   },
-}, (err) => { if (err) { throw err } });
+}, (err) => { if (err) { throw err } });*/
+
+server.register(HapiAuthCookie, (err) => {
+  if (err) {
+    console.log(err)
+  }
+
+  server.auth.strategy('default', 'cookie', {
+    password: 'minimum-32-characters-password1234567890',
+    redirectTo: '/login',
+    isSecure: false
+  });
+
+  server.auth.default('default');
+});
 
 server.views({
   engines: {
@@ -64,108 +80,13 @@ server.views({
 
 server.ext('onPreResponse', (request, reply) => {
   if (request.response.isBoom) {
+    console.log(request.response)
     return reply.view('error', request.response);
   }
   reply.continue();
 });
 
-//Routes
-
-server.route({
-  path: '/',
-  method: 'GET',
-  handler: {
-    file: 'templates/index.html'
-  }
-});
-
-server.route({
-  path: '/assets/{path*}',
-  method: 'GET',
-  handler: {
-    directory: {
-      path: './public',
-      listing: false
-    }
-  }
-});
-
-server.route({
-  path: '/cards/new',
-  method: ['GET', 'POST'],
-  handler: newCardHandler
-});
-
-server.route({
-  path: '/cards',
-  method: 'GET',
-  handler: cardsHandler
-});
-
-server.route({
-  path: '/cards/{id}',
-  method: 'DELETE',
-  handler: deleteCardHandler
-});
-
-var cardSchema = Joi.object().keys({
-  name: Joi.string().min(3).max(50).required(),
-  recipient_email: Joi.string().email().required(),
-  sender_name: Joi.string().min(3).max(50).required(),
-  sender_email: Joi.string().email().required(),
-  card_image: Joi.string().regex(/.+\.(jpg|bmp|png|gif)\b/).required()
-});
-
-
-//Handlers
-
-function newCardHandler(request, reply) {
-  if (request.method === 'get') {
-    reply.view('new', { card_images: map_images() });
-  } else {
-    Joi.validate(request.payload, cardSchema, (err, val) => {
-      if (err) {
-        return reply(Boom.badRequest(err.details[0].message));
-      }
-      const card = {
-        name: val.name,
-        recipient_email: val.recipient_email,
-        sender_name: val.sender_name,
-        sender_email: val.sender_email,
-        card_image: val.card_image
-      };
-
-      saveCard(card);
-      reply.redirect('/cards');
-    });
-  }
-};
-
-function cardsHandler(request, reply) {
-  reply.view('cards', { cards: cards });
-}
-
-function saveCard(card) {
-  const id = uuid.v1();
-  card.id = id;
-  cards[id] = card;
-}
-
-function deleteCardHandler(request, reply) {
-  delete cards[request.params.id];
-  reply();
-}
-
-function loadCards() {
-  let file = fs.readFileSync('./cards.json');
-  return JSON.parse(file.toString());
-}
-
-function map_images() {
-  return fs.readdirSync('./public/images/cards');
-}
-
-
+server.route(Routes);
 
 server.start(() => {
   console.log('Listening on ' + server.info.uri);
